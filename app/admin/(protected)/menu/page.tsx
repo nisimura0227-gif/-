69c7c623a2bae148925e7c2e93e19badeef2ea75
nisimura0227-gif <nisimura0227@gmail.com>
2026-08-05 05:58:42 +0900

    Add files via upload

--- app/admin/(protected)/menu/page.tsx
+++ app/admin/(protected)/menu/page.tsx
@@ -1,5 +1,13 @@
 import ManageList from "@/components/ManageList";
 
 export default function AdminMenuPage() {
-  return <ManageList apiBase="/api/menu" title="🍱 メニュー管理" itemLabel="メニュー" addPlaceholder="例）唐揚げ弁当" />;
+  return (
+    <ManageList
+      apiBase="/api/menu"
+      title="🍱 メニュー管理"
+      itemLabel="メニュー"
+      addPlaceholder="例）唐揚げ弁当"
+      withPrice
+    />
+  );
 }
